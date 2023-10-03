a944a9886e2e14cf56ed3c6ce3f5fcaf86ea2ab5
Amrendra <kumarashishpratp@gmail.com>
2023-10-03 05:39:44 +0530

    Removed unused code

--- BottomActionSheet/ActionSheet/ActionSheet.swift
+++ BottomActionSheet/ActionSheet/ActionSheet.swift
@@ -18,13 +18,6 @@ struct ActionSheet: Identifiable, Hashable {
         let camera = ActionSheet(id: UUID().uuidString, name: "Camera", image: "camera")
         let gallery = ActionSheet(id: UUID().uuidString, name: "Gallery", image: "gallery")
         let document = ActionSheet(id: UUID().uuidString, name: "Document", image: "document")
-        let document1 = ActionSheet(id: UUID().uuidString, name: "Document", image: "document")
-        let document2 = ActionSheet(id: UUID().uuidString, name: "Document", image: "document")
-        let camera1 = ActionSheet(id: UUID().uuidString, name: "Camera", image: "camera")
-        let gallery1 = ActionSheet(id: UUID().uuidString, name: "Gallery", image: "gallery")
-        let camera2 = ActionSheet(id: UUID().uuidString, name: "Camera", image: "camera")
-        let gallery2 = ActionSheet(id: UUID().uuidString, name: "Gallery", image: "gallery")
         return [camera, gallery, document]
     }
-    //, document1, document2, camera1, gallery1, camera2, gallery2, gallery2, gallery2, gallery2, gallery1, gallery1, gallery1, gallery1, document1, document1
 }
